49a4e400378b3f39cb7af8adcf61e475756c2087
Les Aker <me@lesaker.org>
2015-06-06 10:43:20 -0400

    update repo clone url

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -4,7 +4,7 @@ class boxen::repo {
   include boxen::config
 
   #$clone_url  = inline_template('<%= scope.lookupvar("::boxen::config::repo_url_template") % scope.lookupvar("::boxen::config::reponame") %>')
-  $clone_url = inline_template('<%= "https://github.com/%s" % "halyard/halyard" %>')
+  $clone_url = inline_template('<%= "https://github.com/%s" % scope.lookupvar("::boxen::config::reponame") %>')
   $remote_add = "git remote add origin ${clone_url}"
   $git_fetch  = 'git fetch -q origin'
   $git_reset  = 'git reset --hard origin/master'
