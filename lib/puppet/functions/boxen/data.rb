8a9fd87f4a0e7c2b3448cbafbf8fc2648810d5be
Les Aker <me@lesaker.org>
2015-06-06 12:22:32 -0400

    try lookupvar

--- lib/puppet/functions/boxen/data.rb
+++ lib/puppet/functions/boxen/data.rb
@@ -1,21 +1,22 @@
 Puppet::Functions.create_function(:'boxen::data') do
-  def data()
+  def data
+    boxen_home = lookupvar('boxen_home')
     {
-      'boxen::config::home'                   => "#{$::boxen_home}",
-      'boxen::config::bindir'                 => "#{$::boxen_home}/bin",
-      'boxen::config::cachedir'               => "#{$::boxen_home}/cache",
-      'boxen::config::configdir'              => "#{$::boxen_home}/config",
-      'boxen::config::datadir'                => "#{$::boxen_home}/data",
-      'boxen::config::envdir'                 => "#{$::boxen_home}/env.d",
-      'boxen::config::homebrewdir'            => "#{$::homebrew_root}",
-      'boxen::config::logdir'                 => "#{$::boxen_home}/log",
-      'boxen::config::repodir'                => "#{$::boxen_repodir}",
-      'boxen::config::reponame'               => "#{$::boxen_reponame}",
-      'boxen::config::socketdir'              => "#{$::boxen_home}/data/project-sockets",
-      'boxen::config::srcdir'                 => "#{$::boxen_srcdir}",
-      'boxen::config::login'                  => "#{$::github_login}",
-      'boxen::config::repo_url_template'      => "#{$::boxen_repo_url_template}",
-      'boxen::config::download_url_base'      => "#{$::boxen_download_url_base}",
+      'boxen::config::home'                   => boxen_home,
+      'boxen::config::bindir'                 => "#{boxen_home}/bin",
+      'boxen::config::cachedir'               => "#{boxen_home}/cache",
+      'boxen::config::configdir'              => "#{boxen_home}/config",
+      'boxen::config::datadir'                => "#{boxen_home}/data",
+      'boxen::config::envdir'                 => "#{boxen_home}/env.d",
+      'boxen::config::logdir'                 => "#{boxen_home}/log",
+      'boxen::config::socketdir'              => "#{boxen_home}/data/project-sockets",
+      'boxen::config::homebrewdir'            => lookupvar('homebrew_root'),
+      'boxen::config::repodir'                => lookupvar('boxen_repodir'),
+      'boxen::config::reponame'               => lookupvar('boxen_reponame'),
+      'boxen::config::srcdir'                 => lookupvar('boxen_srcdir'),
+      'boxen::config::login'                  => lookupvar('github_login'),
+      'boxen::config::repo_url_template'      => lookupvar('boxen_repo_url_template'),
+      'boxen::config::download_url_base'      => lookupvar('boxen_download_url_base'),
       'boxen::config::hiera_merge_hierarchy'  => false
     }
   end
