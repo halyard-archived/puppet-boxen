8c9d9c725737d5633e3e1bed5c3f2bad00af424c
Les Aker <me@lesaker.org>
2015-06-06 12:37:33 -0400

    try calling out function_

--- lib/puppet/functions/boxen/data.rb
+++ lib/puppet/functions/boxen/data.rb
@@ -1,6 +1,6 @@
 Puppet::Functions.create_function(:'boxen::data') do
   def data
-    boxen_home = lookup('boxen_home')
+    boxen_home = function_lookup('boxen_home')
     {
       'boxen::config::home'                   => boxen_home,
       'boxen::config::bindir'                 => "#{boxen_home}/bin",
@@ -10,13 +10,13 @@ Puppet::Functions.create_function(:'boxen::data') do
       'boxen::config::envdir'                 => "#{boxen_home}/env.d",
       'boxen::config::logdir'                 => "#{boxen_home}/log",
       'boxen::config::socketdir'              => "#{boxen_home}/data/project-sockets",
-      'boxen::config::homebrewdir'            => lookup('homebrew_root'),
-      'boxen::config::repodir'                => lookup('boxen_repodir'),
-      'boxen::config::reponame'               => lookup('boxen_reponame'),
-      'boxen::config::srcdir'                 => lookup('boxen_srcdir'),
-      'boxen::config::login'                  => lookup('github_login'),
-      'boxen::config::repo_url_template'      => lookup('boxen_repo_url_template'),
-      'boxen::config::download_url_base'      => lookup('boxen_download_url_base'),
+      'boxen::config::homebrewdir'            => function_lookup('homebrew_root'),
+      'boxen::config::repodir'                => function_lookup('boxen_repodir'),
+      'boxen::config::reponame'               => function_lookup('boxen_reponame'),
+      'boxen::config::srcdir'                 => function_lookup('boxen_srcdir'),
+      'boxen::config::login'                  => function_lookup('github_login'),
+      'boxen::config::repo_url_template'      => function_lookup('boxen_repo_url_template'),
+      'boxen::config::download_url_base'      => function_lookup('boxen_download_url_base'),
       'boxen::config::hiera_merge_hierarchy'  => false
     }
   end
