003696f54612399b9aa2177db6c5d55e5b45e01c
Les Aker <me@lesaker.org>
2015-06-06 10:19:35 -0400

    update sudoers to use phinze module

--- manifests/sudoers.pp
+++ manifests/sudoers.pp
@@ -1,36 +1,20 @@
-# Private: Ensure we can run Boxen, launchctl, and fdesetup without sudo prompt
+# Private: Ensure we can run Boxen and launchctl without sudo prompt
 
 class boxen::sudoers {
-  sudoers { 'boxen':
-    users    => $::boxen_user,
-    hosts    => 'ALL',
-    commands => [
-      '(ALL) NOPASSWD : /bin/mkdir -p /tmp/puppet',
-      "/bin/mkdir -p ${::boxen_home}",
-      "/usr/sbin/chown ${::boxen_user}\\:staff ${::boxen_home}",
-      "${boxen::config::repodir}/bin/puppet",
-      '/bin/rm -f /tmp/boxen.log'
-    ],
-    type     => 'user_spec',
-  }
+  $boxen_commands = [
+    '/bin/mkdir -p /tmp/puppet',
+    "/bin/mkdir -p ${::boxen_home}",
+    "/usr/sbin/chown ${::boxen_user}:staff ${::boxen_home}",
+    "${boxen::config::repodir}/bin/puppet",
+    '/bin/rm -f /tmp/boxen.log',
+    '/bin/launchctl load',
+    '/bin/launchctl unload'
+  ]
 
-  sudoers { 'fdesetup':
-    users    => $::boxen_user,
-    hosts    => 'ALL',
-    commands => [
-      '(ALL) NOPASSWD : /usr/bin/fdesetup status',
-      '/usr/bin/fdesetup list',
-    ],
-    type     => 'user_spec',
-  }
-
-  sudoers { 'launchctl':
-    users    => $::boxen_user,
-    hosts    => 'ALL',
-    commands => [
-      '(ALL) NOPASSWD : /bin/launchctl load',
-      '/bin/launchctl unload',
-    ],
-    type     => 'user_spec',
+  sudoers::allowed_command{ 'boxen':
+    command          => join($boxen_commands, ','),
+    user             => $::boxen_user,
+    require_password => false,
+    comment          => 'Allows boxen user to manipulate boxen and puppet'
   }
 }
