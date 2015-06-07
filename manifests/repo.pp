c6c7d435bf10e5866a3cb8f0295fce2436f9ee99
Les Aker <me@lesaker.org>
2015-06-06 23:28:37 -0400

    clean up cruft

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -3,8 +3,7 @@
 class boxen::repo {
   include boxen::config
 
-  #$clone_url  = inline_template('<%= scope.lookupvar("::boxen::config::repo_url_template") % scope.lookupvar("::boxen::config::reponame") %>')
-  $clone_url = inline_template('<%= "https://github.com/%s" % scope.lookupvar("::boxen::config::reponame") %>')
+  $clone_url  = inline_template('<%= scope.lookupvar("::boxen::config::repo_url_template") % scope.lookupvar("::boxen::config::reponame") %>')
   $remote_add = "git remote add origin ${clone_url}"
   $git_fetch  = 'git fetch -q origin'
   $git_reset  = 'git reset --hard origin/master'
@@ -17,7 +16,6 @@ class boxen::repo {
   exec { "clone ${boxen::config::home}/repo":
     command => "git init && ${remote_add} && ${git_fetch} && ${git_reset}",
     creates => "${boxen::config::home}/repo/.git",
-    cwd     => "${boxen::config::home}/repo",
-    require => Class['git'],
+    cwd     => "${boxen::config::home}/repo"
   }
 }
