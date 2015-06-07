be209edf146a11ca5e8ebf2133194b03ad25ac9c
Les Aker <me@lesaker.org>
2015-06-07 00:06:49 -0400

    clean up sudoers

--- manifests/sudoers.pp
+++ manifests/sudoers.pp
@@ -4,25 +4,29 @@ class boxen::sudoers {
   $boxen_commands = [
     '/bin/mkdir -p /tmp/puppet',
     "/bin/mkdir -p ${::boxen_home}",
-    "/usr/sbin/chown ${::boxen_user}\:staff ${::boxen_home}",
+    "/usr/sbin/chown ${::boxen_user}\\:staff ${::boxen_home}",
     "${boxen::config::repodir}/bin/puppet",
-    '/bin/rm -f /tmp/boxen.log',
+    '/bin/rm -f /tmp/boxen.log'
+  ]
+
+  $launchctl_commands = [
     '/bin/launchctl load',
     '/bin/launchctl unload'
   ]
 
-  file { '/etc/sudoers.d':
-    ensure => 'directory',
-  } ->
-  file_line { 'include for sudoers.d':
-    path => '/etc/sudoers',
-    line => '#includedir /etc/sudoers.d',
-  } ->
-  sudoers::allowed_command{ 'boxen':
+  sudoers::allowed_command{ 'boxen_repo':
     command          => join($boxen_commands, ','),
     user             => $::boxen_user,
     require_password => false,
     comment          => 'Allows boxen user to manipulate boxen and puppet',
+    require_exist    => false
+  }
+
+  sudoers::allowed_command{ 'boxen_launchct':
+    command          => join($launchctl_commands = ','),
+    user             => $::boxen_user,
+    require_password => false,
+    comment          => 'Allow boxen user to control services',
     require_exist    => false
   }
 }
